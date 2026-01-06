1f18e7e6e2aab1821394ac1665d53c6f76e3e85a
Edward Jazzhands <ed.jazzhands@gmail.com>
2026-01-06 00:17:29 -0800

    Moved zoxide from plugin to tools.zsh

--- oh-my-zsh/tools.zsh
+++ oh-my-zsh/tools.zsh
@@ -2,3 +2,6 @@
 # Note: There is a Homebrew plugin for oh-my-zsh, but it includes a
 # bunch of aliases that I don't want.
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Zoxide
+eval "$(zoxide init bash)"
