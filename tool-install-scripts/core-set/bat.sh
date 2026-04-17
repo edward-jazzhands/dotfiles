00f093c8d31b90cda3d99bcd05a9b074433a6bf7
Edward Jazzhands <ed.jazzhands@gmail.com>
2026-04-16 17:57:56 -0700

    change bat.sh to download binary from github

--- tool-install-scripts/core-set/bat.sh
+++ tool-install-scripts/core-set/bat.sh
@@ -1,6 +1,21 @@
 if command -v batcat &> /dev/null; then
-    echo "batcat is already installed."
+    echo "bat is already installed."
     exit 0
 fi
 
-sudo apt install -y bat
+if command -v bat &> /dev/null; then
+    echo "bat is already installed."
+    exit 0
+fi
+
+# you could use sudo but some of my computers don't allow it
+# sudo apt install -y bat
+
+curl -L -o /tmp/batcat.tar.gz https://github.com/sharkdp/bat/releases/download/v0.26.1/bat-v0.26.1-x86_64-unknown-linux-gnu.tar.gz && \
+  tar -xzf /tmp/batcat.tar.gz -C /tmp && rm /tmp/batcat.tar.gz
+
+cp -r "/tmp/bat-v0.26.1-x86_64-unknown-linux-gnu/bat" "$HOME/.local/bin" 
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp -r "/tmp/bat-v0.26.1-x86_64-unknown-linux-gnu/bat.1" "/usr/local/share/man/man1/bat.1"
+rm -rf "/tmp/bat-v0.26.1-x86_64-unknown-linux-gnu"
+
